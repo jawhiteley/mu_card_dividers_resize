e9d991462509b971f486d646d6d4f14487d2d2d3
Jonathan A. Whiteley <jawhiteley@me.com>
2022-04-12 18:19:27 -0400

    R code cleanup
    Made a copy of experimental code in /archive,
    and removed extra commands from R script.
    R script now checks for existence of key parameters
    before defining them, allowing values from R Markdown file
    to be used on source().
    Associated updates to README, and TODO.

--- TODO.md
+++ TODO.md
@@ -1,16 +1,17 @@
 # To Do
 
-* Tweak and finalize sizes
+* Test and finalize sizes
 * Confirm if there is a loss in image quality.  Try to figure out why and prevent it.
   - only apparent in Inkscape, but could just be anti-aliasing or lack thereof.  Not apparent in Preview?
-* Cleanup unnecessary commands in R, LaTeX
+* Cleanup unnecessary commands in LaTeX
 + Figure out how to handle different paper orientation (season 1 vs X-Men)
 + Automatically check extracted graphics to confirm that they are dividers, and not 'empty space' (X-Men file has pages with <8 dividers on the page for variations and other collections).
   - I should be able to do with by running image_trim() on the extracted image and checking to see if the width is the same or not.  If not, remove it from the list.
 * Collect all the parameters in one place (in a way that is accessible to both R and LaTeX): 
   - Divider height, width, border thickness; run external script; etc.
-* Make R script use parameters from the Rmarkdown file, meaning it could not be run independently.  You can run individual R chunks from the Rmarkdown file, like a notebook, so this should still be functional.  Maybe I'll add a block of necessary parameters in an `if` block so it won't run when source()d, but is still available if running it separately.
+  - Currently not used in R, but now I'm curious, just in case.  LaTeX is the priority.
 + Make output file name based on input file name, to reduce the number of parameters, and make it easier to run on multiple input files?
 + Create a sub-folder for input files that be ignored by git.
     + Allow the program to iterate over a list of input files (i.e., to combine Legion into the main X-Men file, or season 1 and X-Men, in a single pass).
-
++ Convert external script to a user-defined function that can be called directly from the R Markdown file.
+  - That would allow default values for parameters / arguments that can easily be overidden, and avoid having a script that also needs to be able to run stand-alone.
--- mu dividers resize.r
+++ mu dividers resize.r
@@ -1,82 +1,61 @@
 # R v4.1.3        Jonathan Whiteley        2022-04-12
-# Trying to automatically resize Spiffworld's card dividers for Marvel United
+# Extract images from Spiffworld's card dividers for Marvel United
 # https://boardgamegeek.com/filepage/220250/horizontal-card-dividers
 # https://boardgamegeek.com/filepage/228893/horizontal-card-dividers-x-men
 
-# https://www.thepythoncode.com/article/extract-pdf-images-in-python
-# https://rdrr.io/cran/metagear/src/R/PDF_extractImages.R
-# https://www.r-bloggers.com/2016/08/extracting-content-from-pdf-files/
 # https://sckott.github.io/pdfimager/
 # https://cran.r-project.org/web/packages/magick/vignettes/intro.html
 
+
 ## PARAMETERS
-
-# Set Working Directory
-setwd("~/_MyFiles/Play/Board Games/Marvel United/mu_card_dividers_resize")
-
-pdf_file <- "mu_card_dividers_v2.pdf"
-img_dir <- "pdf_images"
-
-# Not used for final output (only experimental code here): see `mu dividers resize.Rmd` for these
-file_out <- "mu_card_dividers_new.pdf"
-new_size <- c(7, 9.2)    # new dimensions of output tabs (in cm): height, width; to specify using inches, use cm(num_inches)
-
-
-## LIBRARIES
-# library(metagear) # requires hexView - ERRORS
-
-
-# Download pdf file? file links are dynamic and expire.
-# download.file(pdf_url, pdf_file)
-
-if (F)
-{
+if (!exists("img_dir") || !exists("pdf_file")) {
+  # Set Working Directory - update to the path to this directory on your system
+  setwd("~/_MyFiles/Play/Board Games/Marvel United/mu_card_dividers_resize")
   
-# Explore downloaded file
-library(pdftools)
-# pdf_info(pdf_file)
-pdf_pages <- pdf_data(pdf_file)
-pdf_attachments(pdf_file)  # no attachments found :(
-# pdf_convert just converts the page to an image: I want the raw image :(
-if (F)
-  pdf_convert(pdf_file, page = 1, format = "png")
-
-# Trying out some of the raw code in metagear pkg
-rawFile <- hexView::readRaw(pdf_file, human = "char")
-# collapse ASCII to a single string
-theStringFile <- paste(hexView::blockValue(rawFile), collapse = '')
-if (F)
-  write.table(theStringFile, "pdf_string.txt", sep = "\n", row.names = FALSE)
-
+  pdf_file <- "mu_card_dividers.pdf"
+  img_dir <- "pdf_images"
 }
 
+library(magrittr)
+
+
+# Download pdf file directly? 
+# BGG file links are dynamic and expire, probably to prevent exactly this type of automated request.
+# download.file(pdf_url, pdf_file)
+
+# Instead, copy the file manually into the working directory (this folder), and set 'pdf_file' to the name of the file
+
+# Check that the pdf file exists, and throw an error if it does not.
+if (file.exists(pdf_file) == FALSE) {
+  stop(sprintf("file '%s' not found: check the 'pdf_file' parameter and working directory.", pdf_file))
+}
 
 
 
 ################################################################
 ## Extract images from pdf file
-# each page is one big image of tabs (created in Photoshop)
+# Each page is one big image of tabs (created in Photoshop)
 
-library(pdfimager)  # remotes::install_github("sckott/pdfimager")    ; requires 'poppler' to be installed on your system (homebrew: brew install poppler)
+library(pdfimager)  # remotes::install_github("sckott/pdfimager")
+# Requires 'poppler' to be installed on your system (homebrew: `brew install poppler`)
 
-pdimg_help()  # check that poppler and pdfimages is installed and accessible.  You will get (command-line) help output if it is, and an error if it's not.
-# extract all images as png files to destination folder (pdfimager automatically creates a sub-folder with the pdf file name).
-# returned value is a table with the relative path to each extracted image file.
+if (F)    # do not run on source()
+  pdimg_help()  # check that poppler and pdfimages is installed and accessible: you will get (command-line) help output if it is, and an error if it's not.
+
+# Extract all images as png files to destination folder (pdfimager automatically creates a sub-folder with the pdf file name).
+# + returned value is a table with the relative path to each extracted image file.
 pdf_img <- pdimg_images(pdf_file, base_dir = img_dir, "-png")
-
-# using pdfimages directly from the command-line works, too: extracts each image as a png to desired location with base name
-# pdfimages -png mu_card_dividers.pdf pdf_images/img
 
 
 
 
 ################################################################
-## Slice extracted images to extract each divider
+## Slice images from each page to extract each divider
 
 library(magick)
-library(magrittr)
 
-# container list to hold image objects for each divider (put something into the first item to make it easier to append() objects in the loop)
+# Initialize a ontainer list to hold image objects for each divider 
+# + put something into the first item to make it easier to append() objects in the loop
 img_list <- list(0)
 
 for (p in 1:nrow(pdf_img[[1]])) {
@@ -104,10 +83,13 @@ for (p in 1:nrow(pdf_img[[1]])) {
   img_list <- append(img_list, image_crop(page, "390x541+1087+544") %>% image_rotate(90) )
 }
 
-# drop first placeholder item
+# Cleanup: drop first placeholder item
 img_list <- img_list[-1]
 
-# export individual dividers as separate images (for use in LaTeX or Rmarkdown?)
+
+#==============================================================#
+# Export individual dividers as separate images (
+# + for use in Rmarkdown / LaTeX
 div_paths <- lapply(1:length(img_list), function (i) {
   div_path <- sprintf("%s/div-%03i.png", img_dir, i)
   image_write(img_list[[i]], path = div_path, format = "png")
@@ -118,39 +100,9 @@ div_paths <- lapply(1:length(img_list), function (i) {
 
 
 ################################################################
-## Layout new pages with resized dividers & output pdf
-if (F) 
-{
-  
-library(grid)
-library(gridExtra)
+## Layout new pages with resized dividers & output pdf?
+# See R Markdown file for final output, using the images output above.
 
-# https://github.com/baptiste/gridextra/wiki/arrangeGrob
-
-div <- rectGrob(width = new_size[2], height = new_size[1], default.units="cm",
-  gp=gpar(col="#000000", fill=NA, lwd=1) )
-page_div <- lapply(1:11, function (x) { div })
-grid.arrange(grobs=page_div, ncol=2)
-marrangeGrob(grobs=page_div, nrow=3, ncol=2)
-
-# use image_graph and image_composite to add space around the original image?
-# sort of works, but not ideal.  I suspect this is changing the resolution of the original image, which is not quite what I want
-div2 <- image_graph(width = 541, height = 541, res = 96)
-grid.arrange(div)
-dev.off()
-div_comp <-image_composite(div2, image_border(img_list[[1]], "black", "1x1"))
-image_border(div_comp, "grey", "2x2")
-
-# open output pdf file: the width & height specify the size of the *graphics region* within the page.
-# Leaving 0.25-inch margin on all sides
-pdf(file=file_out, paper="letter", width = 8, height = 10.5)
-marrangeGrob(page_div, nrow=3, ncol=2)
-lapply(img_list, function(x) { plot(as.raster(x)) } )
-dev.off()
-
-}
-
-# R Markdown & LaTeX will give me more precise control over layout, especially treating each image as an inline box.
+# R Markdown & LaTeX give more precise control over layout and sizing.
 # I also don't trust R's native handling of raster images (see `magick` vignette), 
 #  so I would rather do the resizing in LaTex/Markdown to preserve as much of the original image quality as possible.
-# See R Markdown file for final output.
